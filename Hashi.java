cf82cd2930b987c0bced875eb644acd871682444
ArthurBlr <art.boullier@gmail.com>
2023-02-06 12:13:32 +0100

    Pont et Hashi

---
+++ Hashi.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @author Ilango Remi
+ * @author Jarrier Allan
+ * @author Choux Alex
+ * @author Anna Beranger
+ * @author Boullier Arthur
+ * @author Guimbert Alexis
+ * @author Al Aftan Mohamed
+ * @author Duchesne Thibault
+ */
+public class Hashi {
+
+    public Hashi() {
+    }
+    
+}
--- Pont.java
+++ Pont.java
@@ -1,3 +1,50 @@
+import java.awt.Color;
+
 public class Pont {
-    
+    private Ile ile1;
+    private Ile ile2;
+    private Color couleur;
+
+
+    /**
+     * @param i1 première île
+     * @param i2 deuxième île
+     * @param c couleur du pont
+     */
+    public Pont(Ile i1,Ile i2,Color c){
+        this.ile1 = i1;
+        this.ile2 = i2;
+        this.ile1.ajouterPont();
+        this.ile2.ajouterPont();
+        this.couleur = c;
+    }
+
+    /**
+     * @return la première île liée au pont
+     */
+    public Ile getIle1() {
+        return ile1;
+    }
+
+    /**
+     * @return la deuxième île liée au pont
+     */
+    public Ile getIle2() {
+        return ile2;
+    }
+
+    /**
+     * @return retourne la couleur actuelle du pont
+     */
+    public Color getCouleur() {
+        return couleur;
+    }
+
+
+    /**
+     * @param couleur nouvelle couleur d'un pont
+     */
+    public void setCouleur(Color couleur) {
+        this.couleur = couleur;
+    }
 }
