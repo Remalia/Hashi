033fca31619b77263e4b6082b9530fdaf04afe62
Alexis Guimbert <alexisguimbert16@gmail.com>
2023-02-07 09:50:46 +0100

    push de l'année

--- Fonctionnalite.java
+++ Fonctionnalite.java
@@ -1,4 +1,24 @@
 public class Fonctionnalite{
    Fonctionnalite(){
    } 
+
+   public void undo(){
+   
+   }
+
+   public void redo(){
+
+   }
+
+   public boolean check(){
+
+   }
+
+   public void donnerTechnique(){
+
+   }
+
+   public void hypothese(){
+      
+   }
 }
