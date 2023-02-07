b683a79802a6b12447bc1f47a485b6d593ef7c2c
Beranger Anna <anbberanger@gmail.com>
2023-02-07 11:59:51 +0100

    debut classe surbrillance

--- Surbrillance.java
+++ Surbrillance.java
@@ -1,3 +1,27 @@
+import java.awt.Color;
+
 public class Surbrillance {
-    
+
+    Color c = new Color(255,0,0);
+
+    boolean pontsPotentielsActif;
+    boolean reseauActif;
+
+    public Surbrillance(){
+
+        pontsPotentielsActif = false;
+        reseauActif = false;
+    }
+
+    void reseau(Grille grille, Ile ile){
+        if(reseauActif){
+            ile.setCouleur(c);
+            if(ile.getNbPonts() == 0)
+                return;
+            else{
+            }
+
+        }
+    }
+
 }
