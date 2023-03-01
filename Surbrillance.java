4faaa02c822d55fcab148ded815e10374cd3ab41
ArthurBlr <art.boullier@gmail.com>
2023-03-01 09:14:35 +0100

    commentaires surbrillance

--- Surbrillance.java
+++ Surbrillance.java
@@ -7,12 +7,21 @@ public class Surbrillance {
     boolean pontsPotentielsActif;
     boolean reseauActif;
 
+    /*
+     * Constructeur de Surbrillance
+     */
     public Surbrillance(){
 
         pontsPotentielsActif = false;
         reseauActif = false;
     }
 
+    /*
+     * Active la surbrillance des ponts potentiels
+     * @param grille la grille du jeu
+     * @param ile l'île sur laquelle on veut activer la surbrillance
+     * @return void
+     */ 
     void reseau(Grille grille, Ile ile){
         if(reseauActif){
             ile.setCouleur(c);
