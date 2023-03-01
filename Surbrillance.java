f3976bb0fe902a39b1e55713c68c193aa6b6f19e
Beranger Anna <anbberanger@gmail.com>
2023-03-01 09:52:02 +0100

    surbrillance réseau

--- Surbrillance.java
+++ Surbrillance.java
@@ -1,13 +1,14 @@
 import java.awt.Color;
+import java.util.*;
 
 public class Surbrillance {
 
-    Color c = new Color(255,0,0);
+    Color c = new Color(255,200,200);
 
     boolean pontsPotentielsActif;
     boolean reseauActif;
 
-    /*
+    /** 
      * Constructeur de Surbrillance
      */
     public Surbrillance(){
@@ -16,10 +17,12 @@ public class Surbrillance {
         reseauActif = false;
     }
 
-    /*
-     * Active la surbrillance des ponts potentiels
+    
+
+    /** 
+     * Active la surbrillance du réseau auquel appartient l'île en paramètre
      * @param grille la grille du jeu
-     * @param ile l'île sur laquelle on veut activer la surbrillance
+     * @param ile l'île dont on active 
      * @return void
      */ 
     void reseau(Grille grille, Ile ile){
@@ -28,9 +31,35 @@ public class Surbrillance {
             if(ile.getNbPonts() == 0)
                 return;
             else{
+                /** parcours des ponts de la grille */
+                Iterator<Pont> itr_svg = grille.pileSvg.iterator();
+                while(itr_svg.hasNext()){
+                    Pont pont_cour = itr_svg.next();
+                    /** recherche des ponts reliant l'île ciblée à d'autres îles */
+                    if(pont_cour.getIle1().equals(ile)){
+                        pont_cour.setCouleur(c);
+                        /** 
+                         * si l'île reliée à l'île ciblée n'est pas déjà en surbrillance
+                         * activation de la surbrillance de son réseau 
+                         */
+                        if(!pont_cour.getIle2().getCouleur().equals(c)){
+                            reseau(grille, pont_cour.getIle2());
+                        }
+                    }
+                    if(pont_cour.getIle2().equals(ile)){
+                        pont_cour.setCouleur(c);
+                        /** 
+                         * si l'île reliée à l'île ciblée n'est pas déjà en surbrillance
+                         * activation de la surbrillance de son réseau 
+                         */
+                        if(!pont_cour.getIle1().getCouleur().equals(c)){
+                            reseau(grille, pont_cour.getIle1());
+                        }
+                    }
+                }
             }
-
         }
     }
 
+    
 }
