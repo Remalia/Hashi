78d7eb19fbc667548e48201aa6806d22c583bc39
Alexis Guimbert <97942917+Alexis-42@users.noreply.github.com>
2023-04-02 20:55:10 +0200

    couleurs cercle non final
    Cela permet de mettre en place le système de thème qui change ces couleurs

--- Application/FrontEnd/InterfaceGrille.java
+++ Application/FrontEnd/InterfaceGrille.java
@@ -15,8 +15,8 @@ import java.io.IOException;
 
 public class InterfaceGrille extends Application {
 
-    public static final Color etatNormal = Color.YELLOW;
-    public static final Color etatSelect = Color.GREEN;
+    public static Color etatNormal = Color.YELLOW;
+    public static Color etatSelect = Color.GREEN;
 
     // Dimensions de notre grille et de ces composants
     private final int NB_LIGNES = 10;
