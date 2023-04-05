6ba7085cd0e43918ed81aca6c35fd0ffbc963257
Remalia <Remalia_chan@hotmail.com>
2023-04-05 16:08:29 +0200

    Avancement des commandes et du undo / redo

--- Application/BackEnd/Commandes/Action.java
+++ Application/BackEnd/Commandes/Action.java
@@ -1,16 +1,35 @@
 package Application.BackEnd.Commandes;
 import Application.BackEnd.Grille.Plateau;
+import Application.BackEnd.Grille.Pont;
 
 public abstract class Action {
-    public Plateau plateau;
+    private Plateau plateau;
 
+    /**
+     * Créer une Action
+     * @param plateau Le plateau sur lequel elle s'éxecute
+     */
     public Action(Plateau plateau){
         this.plateau = plateau;
     }
 
+    /**
+     * Execute l'action
+     */
     public abstract void execute();
 
-    public void ecrireAction(){
-        //TODO ajouter a la fin du fichier l'action
-    }
+    /**
+     * Annule l'action
+     */
+    public abstract void undo();
+
+    /**
+     * Permet d'écrire l'action dans un fichier YAML
+     */
+    public abstract void ecrireAction(boolean svg);
+
+    /**
+     * Permet de lire l'action dans un fichier YAML
+     */
+    public abstract Action lireAction(String ligne);
 }
---
+++ Application/BackEnd/Commandes/ActionHistory.java
@@ -0,0 +1,38 @@
+package Application.BackEnd.Commandes;
+
+import java.util.Stack;
+
+public class ActionHistory {
+    private Stack<Action> history = new Stack<>();
+
+    /**
+     * Ajoute en haut de la pile une action
+     * @param a l'action a ajouter.
+     */
+    public void push(Action a){
+        history.push(a);
+    }
+
+    /**
+     * Permet d'enlever l'action le plus haut de la stack d'historique et la renvoie
+     * @return l'action pop
+     */
+    public Action pop(){
+        return history.pop();
+    }
+
+    /**
+     * Permet de vérifier si la pile d'historique est vide ou non
+     * @return True / False en fonction de si la pile est vide
+     */
+    public boolean isEmpty(){
+        return history.isEmpty();
+    }
+
+    /**
+     * Permet de vider la pile d'historique
+     */
+    public void clear(){
+        history.clear();
+    }
+}
