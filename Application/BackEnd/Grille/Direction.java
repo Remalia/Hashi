be61d87f1ff769a2aad501febe93029c32a67a69
Alexis Guimbert <alexisguimbert16@gmail.com>
2023-04-06 14:16:25 +0200

    Enums Orientation & Direction

--- Application/BackEnd/Grille/Direction.java
+++ Application/BackEnd/Grille/Direction.java
@@ -2,4 +2,23 @@ package Application.BackEnd.Grille;
 
 public enum Direction {
     HAUT,BAS,GAUCHE,DROITE;
+
+    public Direction getInverse() {
+        return switch (this) {
+            case HAUT -> BAS;
+            case DROITE -> GAUCHE;
+            case BAS -> HAUT;
+            case GAUCHE -> DROITE;
+            default -> null;
+        };
+    }
+
+    public Orientation getOrientation(Direction d) {
+        return switch (d) {
+            case HAUT, BAS -> Orientation.VERTICAL;
+            case GAUCHE, DROITE -> Orientation.HORIZONTAL;
+            default -> null;
+        };
+    }
+
 }
