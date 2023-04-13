34963aab19b80ff01880ee39f7c14cdb4edcdc6d
Remalia <Remalia_Chan@hotmail.com>
2023-04-13 03:03:44 +0200

    Ajout d'une fonction de récupération de tous les niveaux du jeu

--- Application/BackEnd/Sauvegarde/Paramètre.java
+++ Application/BackEnd/Sauvegarde/Paramètre.java
@@ -1,7 +1,7 @@
 package Application.BackEnd.Sauvegarde;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
+import java.util.*;
 
 public class Paramètre {
     private static final File levelDirectories = new File("Application/Niveau");
@@ -12,23 +12,33 @@ public class Paramètre {
      */
     public static void mettreJeuAZero(boolean uniquementClassement) throws IOException {
         String regex = "^S.*";
-        if(uniquementClassement)
+        if (uniquementClassement)
             regex = "Score.*";
-        for(File dirNiveau : levelDirectories.listFiles()){
-            if(dirNiveau.isDirectory() && dirNiveau.getName().matches("Niveaux.*")){
-                for(File niveau : dirNiveau.listFiles()){
-                    if(niveau.isDirectory() && niveau.getName().matches("Niveau[0-9]+")){
-                        for (File fileNiveau: niveau.listFiles()) {
-                            if(fileNiveau.getName().matches(regex)){
-                                FileWriter eraser = new FileWriter(fileNiveau,false);
-                                eraser.flush();
-                                eraser.close();
-                            }
-                        }
+        for (File fileNiveau : recupAllNiveau()) {
+            if (fileNiveau.getName().matches(regex)) {
+                FileWriter eraser = new FileWriter(fileNiveau, false);
+                eraser.flush();
+                eraser.close();
+            }
+        }
+    }
+
+    /**
+     * Permet de récupérer tous les niveaux du jeu
+     * @return Une liste avec tous les niveaux du jeu
+     */
+    public static List<File> recupAllNiveau() {
+        ArrayList<File> fileNiveaux = new ArrayList<>();
+        for (File dirNiveau : levelDirectories.listFiles()) {
+            if (dirNiveau.isDirectory() && dirNiveau.getName().matches("Niveaux.*")) {
+                for (File niveau : dirNiveau.listFiles()) {
+                    if (niveau.isDirectory() && niveau.getName().matches("Niveau[0-9]+")) {
+                        fileNiveaux.add(niveau);
                     }
                 }
             }
         }
+        return fileNiveaux;
     }
 
     public static void main(String[] args) throws IOException {
